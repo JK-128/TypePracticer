ebc9d490404e09587ab95013c4d05d257982d534
JoeKeep <19701770@students.lincoln.ac.uk>
2023-04-02 15:40:40 +0100

    Updated program so user now has to type the displayed word without seeing their input. After typing the word they are told if they got it correct or incorrect. This process can also be interrupted to configure the program (change difficulty or exit).

--- TypePractice/Source.cpp
+++ TypePractice/Source.cpp
@@ -10,18 +10,12 @@ int main()
 
 	sortValues(&data.values, data.levelCount, true);
 
-	int input =  0;
+	int input =  47;
 	int level = data.levelCount - 1;
 
 	while (input != 27) 
 	{
-		int index = selectWord(&data.values, level);
-
-		std::cout << "The word is: " << data.words[index] << "\n";
-
-		input = _getch();
-
-		if (input == 32)
+		if (input == 47)
 		{
 			input = setDifficulty(data.levelCount);
 
@@ -31,10 +25,44 @@ int main()
 				level = (char)input - 48;
 			}
 		}
+
+		int index = selectWord(&data.values, level);
+
+		std::cout << "The word is: " << data.words[index] << "\n";
+
+		/*
+		std::string attempt;
+
+		std::cout << ">> ";
+		std::getline(std::cin, attempt);
+
+
+		*/
+
+		input = -1;
+
+		std::string attempt = "";
+		while (input != 32 && input != 47 && input != 27 && input != 13)
+		{
+			input = _getch();
+
+			if(input != 32 && input != 13 && input != 47)
+				attempt.push_back((char)input);
+		}
+		std::cout << "Attempt: [" << attempt << "]\n";
+
+		if (attempt == data.words[index])
+			std::cout << "\nCorrect!\n\n";
+		else
+			std::cout << "\nIncorrect\n\n";
+
+		//input = _getch();
+
 	}
 }
+// Try _getch() in a loop to get each character as it is typed. Then exit the loop after the space key had been pressed.
+// Hopefully this method will 1. work and 2. be easily expandible for more than one word.
 
-// TO DO: Change the difficulty classifier script to include at the top of the file the number of difficulty levels.
 // TO DO: Figure out how to take in input via typing words that match the chosen random word.
 // TO DO: String the above along to test for 100 words, then allow for a variable target.
 // TO DO: Put in performance trackers (time, mistakes, etc).
